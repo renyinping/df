fe78fdab83298fcbaf43e96d895abe16106c9493
179055016 <179055016@qq.com>
2020-07-12 20:24:49 +0800

    .

--- qbittorrent/run.sh
+++ qbittorrent/run.sh
@@ -1,19 +1,19 @@
-dir=/srv/dev-disk-by-label-SHARE/share/dl/qbittorrent
-
 docker pull gists/qbittorrent
 
 docker run \
-  --name=qbittorrent \
+  --name qbittorrent \
+  --network host \
   -e UID=1000 \
   -e GID=1000 \
   -e TZ=Asia/Chongqing \
   -e UMASK_SET=022 \
   -e WEB_PORT=81 \
-  -p 81:81 \
-  -p 6881:6881 \
-  -p 6881:6881/udp \
-  -v /$dir:/data \
+  -v /srv/dev-disk-by-label-SHARE/share/dl/qbittorrent:/data \
   -d gists/qbittorrent
+
+  # -p 81:81 \
+  # -p 6881:6881 \
+  # -p 6881:6881/udp \
 
   # webui为ip:81，默认用户名/密码为admin/adminadmin
 
